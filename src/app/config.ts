b76fa556a1b2b41d4dccb10ce635666c71976a29
Garvit Banthiya <113850995+GarvitBanthiya@users.noreply.github.com>
2025-04-19 01:16:58 +0530

    Revert "first commit"

--- src/app/config.ts
+++
@@ -1,9 +0,0 @@
-export const TokenKey = 'authToken';
-export enum Role {
-admin = 'Admin',
-householder = 'Householder',
-serviceProvider = 'ServiceProvider'
-}
-export const BaseUrl = 'http://localhost:8080';
-export const ApiUrlWithUser = '/api/user'
-export const PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
